30627b1ab3af10f38ea4d3facebbc10629bade64
tonnas <illotomas0@gmail.com>
2016-11-21 21:55:56 +0100

    pridanie scriptov

--- scripts/Funkcie.sql
+++ scripts/Funkcie.sql
@@ -3,20 +3,72 @@ Project		Fakturacny system
 Author		Tomas Illo
 */
 
+/*
+	Funkcia pre vypocet celkovej ceny
+*/
+create or replace FUNCTION cena_faktury
+	( p_tel_c IN TEL_CISLO.TEL_CISLO%TYPE,
+    p_odkedy in date,
+    p_dokedy in date )
+RETURN integer
+IS 
+ cena integer;
+ min_v integer;
+ min_m integer;
+ sms_v integer;
+ sms_m integer;
+ mms_v integer;
+ mms_m integer;
+ internet integer;
+BEGIN
+   VYT_POM_TABULIEK(p_tel_c, p_odkedy, p_dokedy);
+   --Odcitat hodnoty z parovej sluzby
+   internet := INTERNET_DATA(p_tel_c, p_odkedy, p_dokedy) 
+            - INTERNET_DATA_SLUZBA(p_tel_c, p_odkedy, p_dokedy);
+   IF (internet < 0) THEN internet := 0; END IF;
+   /*
+      minuty_v = sucet_minut_v_sieti ();
+      minuty_mimo = sucet_minut_mimo ();
+      sms_v = sucet_sms_v_sieti ();
+      sms_mimo = sucet_sms_mimo ();
+      mms_v = sucet_mms_v_sieti ();
+      mms_mimo = sucet_mms_mimo ();
+   */
+   cena := min_v + min_m + sms_v + sms_m + mms_v + mms_m + internet;
+   return cena;
+END;
+
+/*
+	precerpane internet data 
+*/
 CREATE OR REPLACE FUNCTION internet_data
 	(p_tel_c IN HISTORIA_INTERNET.TEL_CISLO%TYPE,
     odkedy in date,
     dokedy in date)
 RETURN integer
 IS 
+d_MB_S integer,
+d_MB_H integer,
 d_MB integer;
 BEGIN
-  SELECT SUM(DATA_MB) INTO d_MB From HISTORIA_INTERNET
-  where TEL_CISLO = p_tel_c 
-  AND DATUM > odkedy 
-  AND DATUM < dokedy;
-  return d_MB; 
-END;	
+  SELECT SUM(DATA_MB) into d_MB_S From INTERNET_SLUZBA
+  JOIN SLUZBA ON SLUZBA.ID_SLUZBY = INTERNET_SLUZBA.ID_INTERNET
+  JOIN TEL_CISLO_SLUZBA ON TEL_CISLO_SLUZBA.ID_SLUZBY = SLUZBA.ID_SLUZBY
+  where TEL_CISLO = p_tel_c;
+  AND DATUM > p_odkedy 
+  AND DATUM < p_dokedy;
+  
+  SELECT SUM(DATA_MB) into d_MB_H From HISTORIA_INTERNET
+  where TEL_CISLO = p_tel_c;
+  AND DATUM > p_odkedy 
+  AND DATUM < p_dokedy;
+  
+  d_MB := d_MB_H - d_MB_S;
+  IF (d_MB >= 0) THEN
+	return d_MB; 
+  else
+	return 0;
+END;
 
 /*
 	Sucet ceny vsetkych sluzieb konkretneho cisla
@@ -58,3 +110,24 @@ BEGIN
   return cRet; 
 END;
 	
+/*
+	Zisti ci su cisla u rovnakeho operatora
+*/	
+create or replace FUNCTION opertor_tc
+	(p_tel_c IN TEL_CISLO.TEL_CISLO%TYPE,
+   p_prijimatel IN TEL_CISLO.TEL_CISLO%TYPE)
+RETURN integer
+IS
+ id_tc integer;
+ id_pr integer;
+BEGIN
+  select id_operator into id_tc from TEL_CISLO_OPERATOR
+  where TEL_CISLO = p_tel_c;
+  select id_operator into id_pr from TEL_CISLO_OPERATOR
+  where TEL_CISLO = p_prijimatel;
+  IF ( id_tc = id_pr ) THEN
+    return 1;
+  ELSE 
+    return 0;
+  END IF;
+END;
